3667ad4ef2c0d2bcb7fd86587ab7fdc1d80e99b1
chiajunjie-digipen <122497239+chiajunjie-digipen@users.noreply.github.com>
2024-11-16 03:36:34 +0800

    this has nothing to do with GAM its just holger stuff

---
+++ Assets/Scripts/EnemyActions.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JackActions : ScriptableObject
+{
+    public AnimationClip idle;
+    public AnimationClip stun;
+}
